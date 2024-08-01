73f838b07c37aafc41f621c0a9ef23dd94a548a6
SergeiKiss <kiselev_s.d.98@mail.ru>
2024-08-01 09:03:02 +0300

    create simple contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract SimpleStorage {
+    uint256 favNum;
+
+    mapping(string => uint256) public nameToFavNum;
+
+    struct People {
+        uint256 favNum;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favNum) public {
+        favNum = _favNum;
+    }
+
+    function retreive() public view returns(uint256){
+        return  favNum;
+    }
+
+    function addPerson(string memory _name, uint256 _favNum) public {
+        people.push(People(_favNum, _name));
+        nameToFavNum[_name] = _favNum;
+    }
+}
