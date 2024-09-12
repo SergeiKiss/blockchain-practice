31294a520b9db04be0718305b70bdf5b4923564f
SergeiKiss <kiselev_s.d.98@mail.ru>
2024-09-12 07:27:47 +0300

    Remix Fund Me

---
+++ FundMe.sol
@@ -0,0 +1,55 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum fundig value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    error NotOwner();
+
+    uint256 public constant MIN_USD = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MIN_USD, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderI = 0; funderI < funders.length; funderI++) {
+            address funder = funders[funderI];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if (msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    receive() external payable { 
+        fund();
+    }
+
+    fallback() external payable { 
+        fund();
+    }
+}
